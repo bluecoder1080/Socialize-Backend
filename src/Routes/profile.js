97b6876702059e0ebe75f02c20a1e13c47f9d0a5
adityasingh1080z@gmail.com <adityasingh1080z@gmail.com>
2025-11-26 16:17:19 +0530

    Refactor authentication middleware and integrate profile route

--- src/Routes/profile.js
+++ src/Routes/profile.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const profileRouter = express.Router();
+const { Userauth } = require("../middlewares/auth");
+
+//Profile Section
+profileRouter.get("/profile", Userauth, async (req, res) => {
+  try {
+    const user = req.user;
+    res.send(user);
+  } catch (err) {
+    res.send("The error is " + err.message);
+  }
+});
+
+module.exports = profileRouter;
