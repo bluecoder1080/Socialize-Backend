bbe72b9e3acfc88580b1854d2a5df5f1c90b0677
adityasingh1080z@gmail.com <adityasingh1080z@gmail.com>
2026-04-13 00:33:07 +0530

    Refactor sendConnectionRequest route to include status and user ID parameters, add status validation, and save connection requests to the database

--- src/Routes/sendConnectionRequest.js
+++ src/Routes/sendConnectionRequest.js
@@ -1,12 +1,33 @@
-const express = require("express")
+const express = require("express");
 const sendConnectionRequestRouter = express.Router();
 const { Userauth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
-sendConnectionRequestRouter.post("/sendConnectionRequest", Userauth, (req, res) => {
-  const user = req.user;
-  console.log("Sending A connection Request");
+sendConnectionRequestRouter.post(
+  "/sendConnectionRequest/:status/:toUserId",
+  Userauth,
+  async (req, res) => {
+    const fromUserId = req.user._id;
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
 
-  res.send(user.FirstName + " Sent The Connection request !!");
-});
+    const statusValidation = ["ignored", "interested"];
+    if (!statusValidation.includes(status)) {
+      res.status(400).json({
+        message: "Invalid Status:  " + status,
+      });
+    }
+
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status,
+    });
+    const data = await connectionRequest.save();
+    res.json({
+      message: "Connection Request Sent !",
+    });
+  },
+);
 
 module.exports = sendConnectionRequestRouter;
