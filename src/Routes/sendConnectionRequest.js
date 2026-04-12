5e5cdaeb4095086a21ab7dee1921685fe85d5ab4
adityasingh1080z@gmail.com <adityasingh1080z@gmail.com>
2026-04-13 02:05:04 +0530

    Refactor sendConnectionRequest route to improve error handling and code structure

--- src/Routes/sendConnectionRequest.js
+++ src/Routes/sendConnectionRequest.js
@@ -7,32 +7,41 @@ sendConnectionRequestRouter.post(
   "/request/send/:status/:toUserId",
   Userauth,
   async (req, res) => {
-    const fromUserId = req.user._id;
-    const toUserId = req.params.toUserId;
-    const status = req.params.status;
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
 
-    const statusValidation = ["ignored", "interested"];
-    if (!statusValidation.includes(status)) {
-      res.status(400).json({
-        message: "Invalid Status:  " + status,
+      const statusValidation = ["ignored", "interested"];
+      if (!statusValidation.includes(status)) {
+        res.status(400).json({
+          message: "Invalid Status:  " + status,
+        });
+      }
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId }, // A → B
+          { fromUserId: toUserId, toUserId: fromUserId }, // B → A
+        ],
       });
-    }
-  const existingConnectionRequest = await ConnectionRequest.findOne({
-  $or: [
-    { fromUserId, toUserId }, // A → B
-    { fromUserId: toUserId, toUserId: fromUserId } // B → A
-  ],
-});
+      if (existingConnectionRequest) {
+        return res.status(400).send({
+          message: "Connection Request Allready Exists",
+        });
+      }
 
-    const connectionRequest = new ConnectionRequest({
-      fromUserId,
-      toUserId,
-      status,
-    });
-    const data = await connectionRequest.save();
-    res.json({
-      message: "Connection Request Sent !",
-    });
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+      const data = await connectionRequest.save();
+      res.json({
+        message: "Connection Request Sent !",
+      });
+    } catch (err) {
+      res.send("the error message -" + err);
+    }
   },
 );
 
