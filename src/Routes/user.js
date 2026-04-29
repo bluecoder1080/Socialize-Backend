3c381736fedf767c0fcb1928caeedb7d2e39316f
adityasingh1080z@gmail.com <adityasingh1080z@gmail.com>
2026-04-29 17:33:49 +0530

    Add feed API endpoint with error handling

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -48,4 +48,14 @@ userRouter.get("/user/connections", Userauth, async (req, res) => {
   }
 });
 
+// Feed Api
+userRouter.get("/feed", Userauth, async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
 module.exports = userRouter;
